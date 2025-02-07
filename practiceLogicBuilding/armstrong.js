5157a7356125e719e5097cd3d866118c91979b32
vkoshta92 <114298415+vkoshta92@users.noreply.github.com>
2025-02-07 15:01:59 +0530

    other method

--- practiceLogicBuilding/armstrong.js
+++ practiceLogicBuilding/armstrong.js
@@ -28,3 +28,23 @@ console.log(isArmstrong(9475)); // false
 console.log(isArmstrong(371));  // true
 console.log(isArmstrong(407));  // true
 console.log(isArmstrong(123));  // false
+
+
+
+//other method
+function isArmstrong(num) {
+    let strNum = num.toString();
+    let digits = strNum.length;
+    let sum = 0;
+
+    for (let char of strNum) {
+        sum += Number(char) ** digits;
+    }
+
+    return sum === num;
+}
+
+console.log(isArmstrong(153));  // true
+console.log(isArmstrong(9474)); // true
+console.log(isArmstrong(123));  // false
+
